2fbde6038e84a03712871667dc7bc14401eeef81
Sergey Shumilin <sergey.s.shumilin@gmail.com>
2024-06-02 10:53:40 +0100

    task 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,16 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const getBooks = () => books;
+const getBookByIsbn = (isbn) => books[isbn];
+const getBooksByAuthor = (author) => Object.entries(books)
+  .reduce((acc, [isbn, cur]) => {
+    return cur.author === author ? ({...acc, [isbn]: cur}) : acc;
+  }, {});
+const getBooksByTitle = (title) => Object.entries(books)
+  .reduce((acc, [isbn, cur]) => {
+    return cur.title === title ? ({...acc, [isbn]: cur}) : acc;
+  }, {});
 
 public_users.post("/register", (req,res) => {
   const newUser = {
@@ -26,34 +36,29 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
+public_users.get('/',async function (req, res) {
+  const books = await getBooks();
+
   return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const book = books[req.params.isbn];
+public_users.get('/isbn/:isbn',async function (req, res) {
+  const book = await getBookByIsbn(req.params.isbn);
   return book ? res.status(200).send(JSON.stringify(book, null, 4))
     : res.status(404).send('Not found');
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const booksByAuthor = Object.entries(books)
-    .reduce((acc, [isbn, cur]) => {
-      return cur.author === req.params.author ? ({...acc, [isbn]: cur}) : acc;
-    }, {});
+public_users.get('/author/:author',async function (req, res) {
+  const booksByAuthor = await getBooksByAuthor(req.params.author);
 
   return res.status(200).send(JSON.stringify(booksByAuthor, null, 4));
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  const booksByTitle = Object.entries(books)
-    .reduce((acc, [isbn, cur]) => {
-      return cur.title === req.params.title ? ({...acc, [isbn]: cur}) : acc;
-    }, {});
+public_users.get('/title/:title',async function (req, res) {
+  const booksByTitle = await getBooksByTitle(req.params.title);
 
   return res.status(200).send(JSON.stringify(booksByTitle, null, 4));
 });
